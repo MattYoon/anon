bb7b19dc4af0cc1a80bddad459c3c6213b6bfef5
MattYoon <mattyoon99@gmail.com>
2025-05-22 13:51:28 +0900

    init

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
-# anon
+# Reasoning Models Better Express Their Confidence
+
+### Summary
+🙁 LLMs are overconfident even when they are dead wrong. 
+
+🧐 What about reasoning models? Can they actually tell us “My answer is only 60% likely to be correct”?
+
+❗Our paper suggests that they can! Through extensive analysis, we investigate what enables this emergent ability.
+
+---
+## Installation
+```
+# clone the repository
+pip install -e lm-eval-harness
+pip install -e evalchemy
+pip install vllm
+```
+
+## Section 3: Main Experiment
+
+1. For **reasoning models** that reliably generate "Confidence Reasoning":
+```
+bash evalchemy/scripts/reasoning_no_force.sh
+```
+
+2. For **reasoning models** that do not reliably generate "Confidence Reasoning" (R1 Distill, OR1-Preview, GLM Z1):
+```
+bash evalchemy/scripts/reasoning_force.sh
+```
+
+3. For **non-reasoning models**:
+```
+bash evalchemy/scripts/non_reasoning.sh
+```
+
+4. Finally, use the notebook `results/calculate_metrics.ipynb` to calculate ECE, Brier Score, and AUROC for the outputs.
+
+---
+## Section 4: Analysis
+### Section 4.1: Linear Regression
+1. For **reasoning models**:
+```
+bash evalchemy/reasoning_slope.sh
+```
+
+2. For **non-reasoning models**:
+```
+bash evalchemy/non_reasoning_slope.sh
+```
+
+3. Finally, use the notebook `results/linear_regression.ipynb` to run linear regression on the calibration metrics.
+
+**Change the dataset path and the model name appropriately referring to the list below.**
+
+<details>
+  <summary>Paths to the segmented CoTs</summary>
+
+  <b>Reasoning Models</b>  
+  - datasets/slopes/reasoning/qwen3-think-nonambigqa-slope.jsonl  
+  - datasets/slopes/reasoning/qwen3-think-triviaqa-slope.jsonl  
+  - datasets/slopes/reasoning/r1-nonambigqa-slope.jsonl  
+  - datasets/slopes/reasoning/r1-triviaqa-slope.jsonl  
+  - datasets/slopes/reasoning/exaone-deep-nonambigqa-slope.jsonl  
+  - datasets/slopes/reasoning/exaone-deep-triviaqa-slope.jsonl  
+  - datasets/slopes/reasoning/glm-z1-nonambigqa-slope.jsonl  
+  - datasets/slopes/reasoning/glm-z1-triviaqa-slope.jsonl  
+
+
+  <b>Non-Reasoning Models</b>  
+  - datasets/slopes/reasoning/qwen3-non-think-nonambigqa-slope.jsonl  
+  - datasets/slopes/reasoning/qwen3-nonthink-triviaqa-slope.jsonl  
+  - datasets/slopes/reasoning/glm-instruct-nonambigqa-slope.jsonl  
+  - datasets/slopes/reasoning/glm-instruct-triviaqa-slope.jsonl  
+  - datasets/slopes/reasoning/exaone-instruct-nonambigqa-slope.jsonl  
+  - datasets/slopes/reasoning/exaone-instruct-triviaqa-slope.jsonl  
+  - datasets/slopes/reasoning/qwen25-nonambigqa-slope.jsonl  
+  - datasets/slopes/reasoning/qwen25-triviaqa-slope.jsonl  
+</details>
+
+### Section 4.2: Ablation
+```
+bash evalchemy/reasoning_ablations.sh
+```
+
+The code used to create the ablated CoTs are available in `ablation_data/`.
+
+### Section 4.3: In-context Slow Thinking
+```
+bash evalchemy/non_reasoning_slow_think.sh
+```
+The few-shot slow thinking examples are available in `evalchemy/eval/chat_benchmarks/non_reasoning_slow_think/few_shot_prompt.py`
+
---
+++ evalchemy/scripts/reasoning_ablations.sh
@@ -0,0 +1,77 @@
+export VLLM_ALLOW_LONG_MAX_MODEL_LEN=1
+export CUDA_VISIBLE_DEVICES=2,3
+export VLLM_USE_V1=0
+export VLLM_WORKER_MULTIPROC_METHOD=spawn
+
+export DS_SIZE=1000
+
+export DS_PATH=datasets/main/triviaqa_val_1k.jsonl
+echo "Running dataset: $DS_PATH, Size: $DS_SIZE"
+OUTPUT_SUBDIR=${DS_PATH#*/}
+
+python -m eval.eval \
+    --model vllm \
+    --tasks reasoning_no_cot\
+    --model_args pretrained=deepseek-ai/DeepSeek-R1-Distill-Qwen-32B,tensor_parallel_size=2,dtype="bfloat16",enforce_eager=True,gpu_memory_utilization=0.90,max_model_len=8192,data_parallel_size=1 \
+    --batch_size auto \
+    --output_path logs/reasoning_ablations/no_cot/$OUTPUT_SUBDIR \
+
+
+export DS_PATH=datasets/main/nonambigqa_val_1k.jsonl
+echo "Running dataset: $DS_PATH, Size: $DS_SIZE"
+OUTPUT_SUBDIR=${DS_PATH#*/}
+
+python -m eval.eval \
+    --model vllm \
+    --tasks reasoning_no_cot\
+    --model_args pretrained=deepseek-ai/DeepSeek-R1-Distill-Qwen-32B,tensor_parallel_size=2,dtype="bfloat16",enforce_eager=True,gpu_memory_utilization=0.90,max_model_len=8192,data_parallel_size=1 \
+    --batch_size auto \
+    --output_path logs/reasoning_ablations/no_cot/$OUTPUT_SUBDIR \
+
+
+export DS_PATH=datasets/ablation/r1-triviaqa-epistemic.jsonl
+echo "Running dataset: $DS_PATH, Size: $DS_SIZE"
+OUTPUT_SUBDIR=${DS_PATH#*/}
+
+python -m eval.eval \
+    --model vllm \
+    --tasks reasoning_pre_cot\
+    --model_args pretrained=deepseek-ai/DeepSeek-R1-Distill-Qwen-32B,tensor_parallel_size=2,dtype="bfloat16",enforce_eager=True,gpu_memory_utilization=0.90,max_model_len=8192,data_parallel_size=1 \
+    --batch_size auto \
+    --output_path logs/reasoning_ablations/epistemic/$OUTPUT_SUBDIR \
+
+
+export DS_PATH=datasets/ablation/r1-nonambigqa-epistemic.jsonl
+echo "Running dataset: $DS_PATH, Size: $DS_SIZE"
+OUTPUT_SUBDIR=${DS_PATH#*/}
+
+python -m eval.eval \
+    --model vllm \
+    --tasks reasoning_pre_cot\
+    --model_args pretrained=deepseek-ai/DeepSeek-R1-Distill-Qwen-32B,tensor_parallel_size=2,dtype="bfloat16",enforce_eager=True,gpu_memory_utilization=0.90,max_model_len=8192,data_parallel_size=1 \
+    --batch_size auto \
+    --output_path logs/reasoning_ablations/epistemic/$OUTPUT_SUBDIR \
+
+
+export DS_PATH=datasets/ablation/r1-triviaqa-non-linear.jsonl
+echo "Running dataset: $DS_PATH, Size: $DS_SIZE"
+OUTPUT_SUBDIR=${DS_PATH#*/}
+
+python -m eval.eval \
+    --model vllm \
+    --tasks reasoning_pre_cot\
+    --model_args pretrained=deepseek-ai/DeepSeek-R1-Distill-Qwen-32B,tensor_parallel_size=2,dtype="bfloat16",enforce_eager=True,gpu_memory_utilization=0.90,max_model_len=8192,data_parallel_size=1 \
+    --batch_size auto \
+    --output_path logs/reasoning_ablations/non-linear/$OUTPUT_SUBDIR \
+
+
+export DS_PATH=datasets/ablation/r1-nonambigqa-non-linear.jsonl
+echo "Running dataset: $DS_PATH, Size: $DS_SIZE"
+OUTPUT_SUBDIR=${DS_PATH#*/}
+
+python -m eval.eval \
+    --model vllm \
+    --tasks reasoning_pre_cot\
+    --model_args pretrained=deepseek-ai/DeepSeek-R1-Distill-Qwen-32B,tensor_parallel_size=2,dtype="bfloat16",enforce_eager=True,gpu_memory_utilization=0.90,max_model_len=8192,data_parallel_size=1 \
+    --batch_size auto \
+    --output_path logs/reasoning_ablations/non-linear/$OUTPUT_SUBDIR \
